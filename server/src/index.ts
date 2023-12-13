cbbd600629ec7daa3103ab6e8d65c8d73b979c77
Danilo Dias <danilolive30@hotmail.com>
2023-12-12 23:39:32 -0300

    feat: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,11 @@ import express, { Express } from "express";
 import rootRouter from "./routes";
 import { PrismaClient } from "@prisma/client";
 import { errorMiddleware } from "./middlewares/errors";
-
+import cors from "cors"
 const app: Express = express();
 
 app.use(express.json())
+app.use(cors())
 app.use("/api", rootRouter);
 
 export const prismaClient = new PrismaClient({
